fbb636bf1652f07e2c9fba74066f4a67ff2ce9c6
Łukasz Ciskowski <lukasz_ciskowski@wp.pl>
2025-03-23 08:21:51 +0100

    feat: config files

--- single-spa-react/src/main.tsx
+++ single-spa-react/src/main.tsx
@@ -7,6 +7,22 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import App from './App.tsx';
 
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-react';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+    // required: the URL of the Grafana collector
+    url: 'http://localhost:8027/collect',
+
+    // required: the identification label of your application
+    app: {
+        name: 'single_spa_react_2',
+        version: '1.0.0',
+        environment: 'production',
+    },
+    instrumentations: [...getWebInstrumentations(), new TracingInstrumentation()],
+});
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
